7bf30904d0b455da3241eea39270a045f4a798b1
league <aiden.person.lundby@gmail.com>
2024-07-22 16:57:38 -0700

    variables

---
+++ _05_for_loops/_1_my_ages/ages.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    year = 2011
+    for i in range(14):
+        print(f"I was {i} in {year} ")
+
+
+
+
+
+        year += 1
+
+
+
---
+++ _05_for_loops/_2_badgers/badge.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    for i in range(2):
+        for i in range(12):
+            print("badger, ")
+        for i in range(2):
+            print("mushroom")
+    print("Snake!")
+
+
+
